10871f01275ddbcf1b9e734a4f66e3c3d65b0f54
Kyrke <martin_matias@hotmail.com.ar>
2022-10-07 15:10:14 -0300

    adding animation and parallax effect

---
+++ assets/scripts/parallax.js
@@ -0,0 +1,6 @@
+let cover = document.querySelector(".relevant-bg");
+
+window.addEventListener("scroll", function(){
+    let value = window.scrollY;
+    cover.style.marginTop = value * 0.3 + 'px';
+})
