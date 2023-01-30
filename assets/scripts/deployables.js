a86363ec09ee14a7798d178dff2072ec0f798b94
Kyrke <martin_matias@hotmail.com.ar>
2023-01-30 17:54:58 -0300

    menu filters finished

--- assets/scripts/deployables.js
+++ assets/scripts/deployables.js
@@ -1,13 +1,15 @@
 var category = document.querySelector('.menu__category');
 var menu = document.querySelector('.menu__deployable');
 var plus = document.querySelector('.fa-plus');
+var minus = document.querySelector('.fa-minus');
 
 
 function show(){
     setTimeout(function showCategory(){
         menu.classList.toggle('menu__deployableShows');
-        plus.classList.toggle('fa-minus');
-    }, 150)
+        plus.classList.toggle('exp__menu');
+        minus.classList.toggle('exp__menu');
+    }, 0)
 }
 
 var menuFilter = document.querySelector('.exp__menu');
@@ -20,4 +22,9 @@ function close(){
     menuFilter.classList.toggle('filters__hidden');
 }
 
+var wrapper = document.querySelector('.wrapper');
+
+function showWrapper(){
+    wrapper.classList.toggle('exp__menu');
+}
 
